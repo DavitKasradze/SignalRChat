a44fa308667b6024fb300d114d3718528a5a6fbb
davit kasradze <dkasradze@velitech.com>
2024-07-10 14:17:46 +0400

    refactored ExceptionFilter

--- ConsoleSignalRServer/Filters/ExceptionFilter.cs
+++ ConsoleSignalRServer/Filters/ExceptionFilter.cs
@@ -10,48 +10,49 @@ public class ExceptionFilter : IHubFilter
     public async ValueTask<object> InvokeMethodAsync(
         HubInvocationContext invocationContext, Func<HubInvocationContext, ValueTask<object>> next)
     {
+        var errorMessage = "ErrorMessage";
         try
         {
             return await next(invocationContext);
         }
         catch (ExceptionMessages.MembershipValidationException ex)
         {
-            await invocationContext.Hub.Clients.Caller.SendAsync("ErrorMessage", ex.Message);
+            await invocationContext.Hub.Clients.Caller.SendAsync(errorMessage, ex.Message);
             return null;
         }
         catch (ExceptionMessages.MutedMemberException ex)
         {
-            await invocationContext.Hub.Clients.Caller.SendAsync("ErrorMessage", ex.Message);
+            await invocationContext.Hub.Clients.Caller.SendAsync(errorMessage, ex.Message);
             return null;
         }
         catch (ExceptionMessages.RoomExistsException ex)
         {
-            await invocationContext.Hub.Clients.Caller.SendAsync("ErrorMessage", ex.Message);
+            await invocationContext.Hub.Clients.Caller.SendAsync(errorMessage, ex.Message);
             return null;
         }
         catch (ExceptionMessages.RoomOwnerValidationException ex)
         {
-            await invocationContext.Hub.Clients.Caller.SendAsync("ErrorMessage", ex.Message);
+            await invocationContext.Hub.Clients.Caller.SendAsync(errorMessage, ex.Message);
             return null;
         }
         catch (ExceptionMessages.SelfRemovalException ex)
         {
-            await invocationContext.Hub.Clients.Caller.SendAsync("ErrorMessage", ex.Message);
+            await invocationContext.Hub.Clients.Caller.SendAsync(errorMessage, ex.Message);
             return null;
         }
         catch (ExceptionMessages.SelfMuteException ex)
         {
-            await invocationContext.Hub.Clients.Caller.SendAsync("ErrorMessage", ex.Message);
+            await invocationContext.Hub.Clients.Caller.SendAsync(errorMessage, ex.Message);
             return null;
         }
         catch (ExceptionMessages.EmptyUsernameException ex)
         {
-            await invocationContext.Hub.Clients.Caller.SendAsync("ErrorMessage", ex.Message);
+            await invocationContext.Hub.Clients.Caller.SendAsync(errorMessage, ex.Message);
             return null;
         }
         catch (ExceptionMessages.NotARoomMemberException ex)
         {
-            await invocationContext.Hub.Clients.Caller.SendAsync("ErrorMessage", ex.Message);
+            await invocationContext.Hub.Clients.Caller.SendAsync(errorMessage, ex.Message);
             return null;
         }
     }
