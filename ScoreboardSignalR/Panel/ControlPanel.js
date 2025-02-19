1fb3d3c24fa11c9f4a15b766cc56d2504efc8fd4
Davit Kasradze <dkasradze@velitech.com>
2025-02-19 18:44:22 +0400

    updated control panel functions, now every button works

--- ScoreboardSignalR/Panel/ControlPanel.js
+++ ScoreboardSignalR/Panel/ControlPanel.js
@@ -32,4 +32,60 @@ document.getElementById("updateScoreboard").addEventListener("click", () => {
         .catch(err => console.error(err));
 });
 
+document.getElementById("newSet").addEventListener("click", () => {
+    document.getElementById("clanPrefixOne").value = "";
+    document.getElementById("nameOne").value = "";
+    document.getElementById("countryOne").value = "Unknown";
+    document.getElementById("scoreOne").value = 0;
+
+    document.getElementById("clanPrefixTwo").value = "";
+    document.getElementById("nameTwo").value = "";
+    document.getElementById("countryTwo").value = "Unknown";
+    document.getElementById("scoreTwo").value = 0;
+
+    document.getElementById("prizePool").value = "";
+    document.getElementById("currentRound").value = "";
+    document.getElementById("upcomingRound").value = "";
+    document.getElementById("upcomingPrefixOne").value = "";
+    document.getElementById("upcomingNameOne").value = "";
+    document.getElementById("upcomingCountryOne").value = "";
+    document.getElementById("upcomingCharacterOne").value = "";
+    document.getElementById("upcomingPrefixTwo").value = "";
+    document.getElementById("upcomingNameTwo").value = "";
+    document.getElementById("upcomingCountryTwo").value = "";
+    document.getElementById("upcomingCharacterTwo").value = "";
+});
+
+document.getElementById("scoreReset").addEventListener("click", () => {
+    document.getElementById("scoreOne").value = 0;
+    document.getElementById("scoreTwo").value = 0;
+});
+
+document.getElementById("swap").addEventListener("click", () => {
+    function swapValues(id1, id2) {
+        let temp = document.getElementById(id1).value;
+        document.getElementById(id1).value = document.getElementById(id2).value;
+        document.getElementById(id2).value = temp;
+    }
+
+    swapValues("clanPrefixOne", "clanPrefixTwo");
+    swapValues("nameOne", "nameTwo");
+    swapValues("countryOne", "countryTwo");
+    swapValues("scoreOne", "scoreTwo");
+});
+
+document.getElementById("clearOne").addEventListener("click", () => {
+    document.getElementById("clanPrefixOne").value = "";
+    document.getElementById("nameOne").value = "";
+    document.getElementById("countryOne").value = "Unknown";
+    document.getElementById("scoreOne").value = 0;
+});
+
+document.getElementById("clearTwo").addEventListener("click", () => {
+    document.getElementById("clanPrefixTwo").value = "";
+    document.getElementById("nameTwo").value = "";
+    document.getElementById("countryTwo").value = "Unknown";
+    document.getElementById("scoreTwo").value = 0;
+});
+
 connection.start().catch(err => console.error(err));
